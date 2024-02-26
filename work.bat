f2c97ae33fc5e45cc7e8edd8e3ca9b2a5a5c9bfc
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2024-02-26 17:57:20 -0500

    Converted work script from CMD to PS1.  Converted 0-REFRESH from CMD to PS1.

--- work.bat
+++
@@ -1,7 +0,0 @@
-REM
-REM $Id: work.bat 5487 2014-11-22 22:50:44Z kevinanlipscomb $
-REM
-cd "C:\Inetpub\wwwroot\PACRAT"
-start /max explorer /e,/select,C:\Inetpub\wwwroot\PACRAT\.svn
-start /max PACRAT.sln
-IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
